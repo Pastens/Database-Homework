12588c22282b15d12299a26f61dc34eddd552e4b
Haocheng Liu <liuhc@staff.zjutv.com>
2016-01-13 17:07:24 +0800

    update 2016.1.13 17:07

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -81,21 +81,25 @@ class IndexController extends Controller {
         	}
         	$this->assign('data',$data);
         }
-        $itemData = M('Products') -> where("enable=1") ->select();
-        $statistic['itemType'] = count($itemData);
-        $statistic['shopNum'] = count(M('Shop')->where("enable=1")->select());
+        $itemData = M('Shop') -> where("enable=1") ->select();
+        $statistic['itemType'] = count(M('Products')->where("enable=1")->select());
+        $statistic['shopNum'] = count($itemData);
 
         for($i=0;$i<count($itemData);++$i){
-        	$item[$i]['itemCover'] = $itemData[$i][coverurl];
-        	$item[$i]['itemName'] = $itemData[$i][productname];
-        	$item[$i]['itemIntro'] = $itemData[$i][productintro];
-        	$item[$i]['itemCount'] = $itemData[$i][productcount];
-        	$item[$i]['itemPrice'] = $itemData[$i][productprice];
-
-        	$item[$i]['itemSales'] = 0;
-        	$sales = M('Orderitems') -> where("productId='%d'",$itemData[$i][productid]) -> select();
-        	for($j=0;$j<count($sales);++$j){
-        		$item[$i]['itemSales'] += $sales[$j][quantity];
+        	$item[$i]['shopName'] = $itemData[$i][shopname];
+        	if($itemData[$i][adminid]){
+        		$item[$i]['shopOwner'] = M('Admin') -> where("adminId='%d'",$itemData[$i][adminid]) -> getField('adminNickname');
+        	}else{
+        		$item[$i]['shopOwner'] = "Eshop自营";
+        	}
+        	$products= M('Products')->where("shopId='%d'",$itemData[$i][shopid])->select();
+        	$item[$i]['shopProducts'] = count($products);
+        	$item[$i]['shopProfits'] = 0;
+        	for($j=0;$j<count($products);++$j){
+        		$sales = M('Orderitems') -> where("productId='%d'",$products[$j][productid]) -> select();
+        		for($k=0;$k<count($sales);++$k){
+        			$item[$i]['shopProfits'] += $sales[$k][quantity] * $products[$j][productprice];
+        		}
         	}
         }
         $this->assign('item',$item);
